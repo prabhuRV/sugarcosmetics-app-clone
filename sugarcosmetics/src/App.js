319e8eef03f7ad510c35c3c52ec9b318812c2181
goutham934 <99542699+goutham934@users.noreply.github.com>
2022-05-03 11:44:45 +0530

    last

--- sugarcosmetics/src/App.js
+++ sugarcosmetics/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Breashes from './components/pages/brashes/breashes';
 
 
 function App() {
   return (
     <div className="App">
      <h1>hello world</h1>
+     <Breashes/>
     </div>
   );
 }
---
+++ sugarcosmetics/src/components/pages/brashes/Breashes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Breashes = () => {
+  return (
+    <div>breashes</div>
+  )
+}
+
+export default Breashes
