b993a13d79cfdf7cb1c2b9ea042b83c951c1900b
prabhuRV <99705782+prabhuRV@users.noreply.github.com>
2022-05-04 17:36:46 +0530

    5.35

--- sugarcosmetics/src/App.js
+++ sugarcosmetics/src/App.js
@@ -1,37 +1,24 @@
 
 import './App.css';
 
-import Breashes from './components/pages/brashes/Breashes';
-import Login from './components/pages/login/Login';
+// import Breashes from './components/pages/brashes/Breashes';
+// import Login from './components/pages/login/Login';
 
-import BreshesData from './components/pages/Breshes/BreshesData';
+// import BreshesData from './components/pages/Breshes/BreshesData';
 
 
 
-<<<<<<< Updated upstream
 
 // // import Face from './components/pages/makeup/Face';
 //  import Lips from './components/pages/makeup/Lips';
 //  import MakeUp from './components/pages/makeup/MakeUp';
-=======
-// import Face from './components/pages/makeup/Face';
- import Lips from './components/pages/makeup/Lips';
- import MakeUp from './components/pages/makeup/MakeUp';
-import MakeupKit from './components/pages/Trending/MakeupKit';
->>>>>>> Stashed changes
 
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
 
-=======
-   {/* <BreshesData /> */}
-   {/* <MakeUp /> */}
-   <MakeupKit />
->>>>>>> Stashed changes
     </div>
   );
 }
