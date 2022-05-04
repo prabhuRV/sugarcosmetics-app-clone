f75a6b48162a1d871ca6682ee038453a37d10c94
Pratik <paulpratik6@gmail.com>
2022-05-04 23:17:47 +0530

    style

--- sugarcosmetics/src/App.js
+++ sugarcosmetics/src/App.js
@@ -1,13 +1,41 @@
 
 import './App.css';
+<<<<<<< Updated upstream
 import { MainRouter } from './components/routes/MainRouter';
+=======
+import BreshesData from './components/pages/Breshes/BreshesData';
+import { BestSellers } from './components/pages/Trending/BestSellers';
+import MakeupKit from './components/pages/Trending/MakeupKit';
+import { MerchStation } from './components/pages/Trending/MerchStation';
+
+
+import Login from './components/pages/login/Login';
+
+// import BreshesData from './components/pages/Breshes/BreshesData';
+
+
+
+
+// // import Face from './components/pages/makeup/Face';
+//  import Lips from './components/pages/makeup/Lips';
+//  import MakeUp from './components/pages/makeup/MakeUp';
+>>>>>>> Stashed changes
 
 
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
         <MainRouter/>
+=======
+      {/* <BreshesData /> */}
+      <Login/>
+      {/* <BestSellers />
+      <MerchStation /> */}
+  
+{/* <MakeupKit /> */}
+>>>>>>> Stashed changes
     </div>
   );
 }
