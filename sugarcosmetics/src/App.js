d1a124ec0b7c65bbbc289a733afdabc5053d6b46
Pratik <paulpratik6@gmail.com>
2022-05-04 18:05:37 +0530

    did login page

--- sugarcosmetics/src/App.js
+++ sugarcosmetics/src/App.js
@@ -1,24 +1,25 @@
 
 import './App.css';
 
-import Breashes from './components/pages/brashes/Breashes';
-import Login from './components/pages/login/Login';
+// import Breashes from './components/pages/brashes/Breashes';
+// import Login from './components/pages/login/Login';
+// import MakeUp from './components/pages/makeup/MakeUp';
 
-import BreshesData from './components/pages/Breshes/BreshesData';
+// import BreshesData from './components/pages/Breshes/BreshesData';
 
 
 
 
 // // import Face from './components/pages/makeup/Face';
 //  import Lips from './components/pages/makeup/Lips';
-//  import MakeUp from './components/pages/makeup/MakeUp';
+
 
 
 
 function App() {
   return (
     <div className="App">
-
+  
     </div>
   );
 }
