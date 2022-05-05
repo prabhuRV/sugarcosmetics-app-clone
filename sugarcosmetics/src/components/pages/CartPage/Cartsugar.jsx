6d3f8aa2b5c776f35eeb7e79588f83b84cc9df71
goutham934 <99542699+goutham934@users.noreply.github.com>
2022-05-05 16:38:59 +0530

    cart page

---
+++ sugarcosmetics/src/components/pages/CartPage/Cartsugar.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import styles from './sugar.css'
+const Cartsugar = () => {
+  return (
+    <>
+     <div className="carTotal">
+          <div className='homeLogo'>
+           <span style={{marginLeft:"35px"}}></span> <i className="fa-solid fa-house"></i> / Cart
+         </div>
+    
+        <div className='itemsDiv'>
+              <div className='gap1'>
+                  <div className='dummy'>
+                  <div>
+                       <img src="https://in.sugarcosmetics.com/desc-images/shopping_cart.svg"width="20px" height="20px" alt="cartLogo" /> 
+                   <span style={{marginLeft:"10px"}}>Order Summary</span>
+                  </div>
+                  <div>
+                      Cart Total : Rs. 
+                  </div>
+                  </div>
+                  <div style={{display:"flex"}} className='dummy'>
+                    <div> <img style={{margin:"0px"}} src="https://in.sugarcosmetics.com/desc-images/Offers_price_details.svg" width="20px" height="20px"  alt="offer"/>
+                     <span style={{marginLeft:"10px",paddingTop:"-5px"}} >Offers and Price Details</span></div>
+                  </div>
+              </div>
+
+           <div style={{display:"flex",justifyContent:"center"}} >
+
+                <div className='half'>
+                     <div>
+                         
+                     </div>
+                     
+                </div>
+               <div className='half1' style={{backgroundColor:"whitesmoke"}}>
+                
+                   <div style={{backgroundColor:"#fff",padding:"10px 20px",marginTop:"15px"}}>
+                        <div style={{height:"35px",paddingTop:"10px"}}>
+                        <img src="https://in.sugarcosmetics.com/desc-images/GiftCard.svg" width="20px" height="20px" ></img>
+                        <span style={{marginLeft:"10px"}}>Gift Card/Discount Code</span>
+                        <input className='promocode' type="text" />
+                        <input className='promosub' type="submit" value="submit" />
+                    </div>
+
+                    <div style={{height:"35px",paddingTop:"11px"}}>
+                        <img src="	https://in.sugarcosmetics.com/desc-images/PriceDetails.svg" width="20px" height="20px" ></img>
+                        <span style={{marginLeft:"10px"}}>Price Details</span>
+                    </div>
+                    
+                    
+                    <div style={{height:"30px",paddingTop:"5px"}}>
+                        <img src="	https://in.sugarcosmetics.com/desc-images/CartSubtotal.svg" width="15px" height="15px" />
+                         <span style={{marginLeft:"10px"}}>Cart Sub Total: </span>
+                         <span  style={{marginLeft:"80px"}}>₹ 50000</span>
+                    </div>
+                      <div style={{height:"30px",paddingTop:"5px"}}>
+                        <img src="https://in.sugarcosmetics.com/desc-images/Shipping_Cost.svg" width="15px" height="15px" />
+                         <span style={{marginLeft:"10px"}}>Shipping Cost: </span>
+                         <span  style={{marginLeft:"80px"}}>₹ 00.0</span>
+                    </div>
+                      <div style={{height:"30px",paddingTop:"5px"}}>
+                        <img src="https://in.sugarcosmetics.com/desc-images/Discount.svg" width="15px" height="15px" />
+                         <span style={{marginLeft:"10px"}}>Discount Applied: </span>
+                         <span  style={{marginLeft:"60px"}}>₹ 00.0</span>
+                    </div>
+                      <div style={{height:"30px",paddingTop:"5px"}}>
+                        <img src="https://in.sugarcosmetics.com/desc-images/AmountPayable.svg" width="15px" height="15px" />
+                         <span style={{marginLeft:"10px"}}>Amount Payable: </span>
+                         <span  style={{marginLeft:"65px"}}>₹ 50000</span>
+                    </div>
+                    <div>
+                        <span>Including ₹ 494.69  in taxes</span>
+                    </div>
+
+                   </div>
+                   <div style={{marginTop:"20px",display:"flex"}}>
+                       <button style={{height:"36.50px",border:"1px solid gray",width:"35%",backgroundColor:"#fff",}}> Continue Shopping</button>
+                       <button style={{height:"37px",border:"1px solid gray",width:"70%",backgroundColor:"darkslategray",color:"#fff"}}>Delivery Information</button>
+                   </div>
+               </div>
+
+           </div>
+
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default Cartsugar
