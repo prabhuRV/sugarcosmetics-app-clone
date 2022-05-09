5aaff012a1ae56dc6b331b09dbe0951e50c9cb95
goutham934 <99542699+goutham934@users.noreply.github.com>
2022-05-09 13:13:15 +0530

    done pop of login in deliveypage

--- sugarcosmetics/src/components/pages/Payment/Delivery.jsx
+++ sugarcosmetics/src/components/pages/Payment/Delivery.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import styles from "./Delivery.module.css";
 
+
 function Delivery() {
   const navigate = useNavigate();
+    const [show, setShow] = React.useState(false);
   return (
     <>
       <div style={{ marginTop: "50px" }}></div>
@@ -15,7 +17,9 @@ function Delivery() {
             src="https://in.sugarcosmetics.com/guestCheckout/topleftcircles75.png"
             alt=""
           />
-          <button className={styles.button1}>Log In/Sign Up</button>
+          <button className={styles.button1} onClick={() => setShow(true)}>
+            Log In/Sign Up
+          </button>
           <p style={{ marginTop: "10px" }}>OR</p>
           <div className={styles.innerdiv1}>
             <img
@@ -87,6 +91,94 @@ function Delivery() {
           />
         </div>
       </div>
+      {show && (
+        <>
+          <div className={styles.bgmodal}>
+            <div className={styles.modalcontents}>
+              <div id={styles.slide}>
+                <img
+                  id={styles.bg}
+                  src="https://in.sugarcosmetics.com/Login_bg.svg"
+                  alt=""
+                />
+              </div>
+
+              <div id={styles.form}>
+                <p id={styles.head}>
+                  <span
+                    style={{ marginRight: "50px", cursor: "pointer" }}
+                    onClick={() => setShow(false)}
+                  >
+                    <i class="fa-solid fa-xmark"></i>
+                  </span>
+                  Login/Sign Up Using Phone
+                </p>
+                <div id={styles.numHold}>
+                  <p id={styles.code}>+91</p>
+                  <input type="text" id={styles.number} maxlength="1" />
+                  <input type="text" id={styles.number} maxlength="1" />
+                  <input type="text" id={styles.number} maxlength="1" />
+                  <input type="text" id={styles.number} maxlength="1" />
+                  <input type="text" id={styles.number} maxlength="1" />
+                  <input type="text" id={styles.number} maxlength="1" />
+                  <input type="text" id={styles.number} maxlength="1" />
+                  <input type="text" id={styles.number} maxlength="1" />
+                  <input type="text" id={styles.number} maxlength="1" />
+                  <input type="text" id={styles.number} maxlength="1" />
+                  <button id={styles.request}>Request OTP</button>
+                </div>
+
+                <p id={styles.enter}>
+                  Enter your phone number to request an OTP
+                </p>
+                <hr id={styles.hr} />
+                <p id={styles.digit}>
+                  Enter the 4 digit OTP received on your phone.
+                </p>
+
+                <div id={styles.otpHold}>
+                  <input type="text" id={styles.otp} maxlength="1" />
+                  <input type="text" id={styles.otp} maxlength="1" />
+                  <input type="text" id={styles.otp} maxlength="1" />
+                  <input type="text" id={styles.otp} maxlength="1" />
+                </div>
+                <button id={styles.verify} onClick={() => navigate("/payment")}>
+                  Verify OTP
+                </button>
+
+                <div id={styles.condition}>
+                  <input type="checkbox" id={styles.check} />
+                  <p id={styles.whatsapp}>
+                    Get Important Updates on Whatsapp.{" "}
+                  </p>
+                  <a id={styles.terms} href="">
+                    Terms and Conditions
+                  </a>
+                </div>
+
+                <div id={styles.extra}>
+                  <p id={styles.p1}>
+                    Registering for this site allows you to access your order
+                    status and history. Just fill in the fields below, and we'll
+                    get a new account set up for you in no time. We will only
+                    ask you for information necessary to make the purchase
+                    process faster and easier.
+                  </p>
+
+                  <div id={styles.extraInfo}>
+                    <p id={styles.p2}>
+                      By Signing up or logging in, you agree to our{" "}
+                    </p>
+                    <a href="" id={styles.terms2}>
+                      Terms and Conditions
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
